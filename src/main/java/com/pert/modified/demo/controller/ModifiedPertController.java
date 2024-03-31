be1059bc68d46186617de287f20631dbacc4edd5
Viktor Shutko <gthoprodakwn@gmail.com>
2024-03-31 11:13:57 +0300

    Update ModifiedPertController request mapping and URL

--- src/main/java/com/pert/modified/demo/controller/ModifiedPertController.java
+++ src/main/java/com/pert/modified/demo/controller/ModifiedPertController.java
@@ -4,7 +4,7 @@ import com.pert.modified.demo.dto.request.CalculationRequest;
 import com.pert.modified.demo.dto.response.CalculationResponse;
 import com.pert.modified.demo.service.ModifiedPertService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ModifiedPertController {
     private final ModifiedPertService modifiedPertService;
 
-    @GetMapping("/calculate")
+    @PostMapping("/calculation")
     public CalculationResponse calculateOperationsDuration(@RequestBody CalculationRequest calculationRequest) {
         return new CalculationResponse(modifiedPertService.calculateOperationsDuration(calculationRequest));
     }
